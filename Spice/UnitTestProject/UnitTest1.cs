2ddbe26893a033cf644e70a5422c9f9866bbda70
UShandruk <ulyana.shandruk@gmail.com>
2015-08-24 17:48:47 +0300

    Добавление библиотеки Triangle.dll

---
+++ Spice/UnitTestProject/UnitTest1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Triangle;
+
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void CalculateTriangleSquare()
+        {
+            //Arrange - подготовка
+            TriangleService triangleService = new TriangleService();
+
+            decimal a = 3;
+            decimal b = 4;
+            decimal c = 5;
+            
+            //Act - выполнение тестируемого действия (вызов тестироемого метода)
+            double result = triangleService.CalculateSquare(a, b, c);
+
+            //Assert - проверка предположения
+            Assert.AreEqual(6, result);
+
+        }
+
+        public void TestMethod2()
+        {
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+        }
+    }
+}
