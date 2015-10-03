b7a375073024ae4d8886511d896692ca2a8cc162
UShandruk <ulyana.shandruk@gmail.com>
2015-10-03 21:41:53 +0300

    .

---
+++ 2. Blog/Blog/Models/IMessagesDbService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Models
+{
+    interface IMessagesDbService
+    {
+        IOrderedQueryable<Message> ExtractMessages();
+      
+        Message FindMessageById(int? id);
+
+        void AddMessage(Message message);
+
+        void ModifyMessage(Message message);
+
+        void RemoveMessage(int Id);
+
+        void SaveChanges();
+    }
+}
---
+++ 2. Blog/Blog/Models/IRepliesDbService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Models
+{
+    interface IRepliesDbService
+    {
+        IQueryable<Reply> ExtractReplies();
+
+        Reply FindReplyById(int? id);
+
+        void AddReply(Reply reply);
+
+        void ModifyReply(Reply reply);
+
+        void RemoveRepliy(Reply reply);
+
+        void SaveChanges();
+    }
+}
