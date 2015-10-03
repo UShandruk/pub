5bc91d6459290da5089c59e5c147090e3866cba5
UShandruk <ulyana.shandruk@gmail.com>
2015-10-03 21:41:18 +0300

    .

---
+++ 1. Phonebook/PhoneBook/PhoneBook/Models/IDbService.cs
@@ -0,0 +1,24 @@
+﻿using PhoneBook.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBook.Models
+{
+    public interface IDbService
+    {
+        List<RecordViewModel> Search(string searching);
+
+        List<RecordViewModel> ShowRecords();
+
+        void Add(RecordViewModel record);
+
+        Record Get(int? Id);
+
+        List<Record> GetAll();
+
+        void Delete(int? Id);
+    }
+}
---
+++ 1. Phonebook/PhoneBook/PhoneBook/Services/DbService.cs
@@ -0,0 +1,61 @@
+﻿using PhoneBook.DAL;
+using PhoneBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PhoneBook.Services
+{
+    public class DbService : IDbService
+    {
+        private PhoneBookDataContext db = new PhoneBookDataContext();
+
+        public List<RecordViewModel> Search(string searching)
+        {
+           return db.Records.Where(x => x.Name.Contains(searching) || x.Surname.Contains(searching) || x.Phone.Contains(searching)).Select(x => new RecordViewModel { Id = x.Id, Name = x.Name, Surname = x.Surname, Phone = x.Phone }).ToList();
+        }
+
+        public List<RecordViewModel> ShowRecords()
+        {
+            return db.Records.Select(x => new RecordViewModel { Id = x.Id, Name = x.Name, Surname = x.Surname, Phone = x.Phone }).ToList();
+        }
+
+        public void Add(RecordViewModel record)
+        {
+            var recordToEdit = db.Records.FirstOrDefault(r => r.Id == record.Id);
+            if (record.Id == 0)
+            {
+                db.Records.InsertOnSubmit(new Record { Surname = record.Surname, Name = record.Name, Phone = record.Phone });
+            }
+            
+            else
+            {
+                recordToEdit.Surname = record.Surname;
+                recordToEdit.Name = record.Name;
+                recordToEdit.Phone = record.Phone;
+            }
+            db.SubmitChanges();
+        }
+
+        public Record Get(int? Id)
+        {
+            var result = db.Records.FirstOrDefault(r => r.Id == Id);
+            return result;                
+        }
+        public List<Record> GetAll()
+        {
+            return db.Records.ToList();
+        }
+
+        public void Delete(int? Id)
+        {
+            var recordToDelete = db.Records.FirstOrDefault(r => r.Id == Id);
+            if (recordToDelete == null)
+                return;
+            
+            db.Records.DeleteOnSubmit(recordToDelete);
+            db.SubmitChanges();
+        }
+    }
+}
