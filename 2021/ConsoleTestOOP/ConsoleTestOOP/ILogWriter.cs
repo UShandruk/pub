e9a153d35c1adb2d93320f8aa49e70f8ed55e4bd
UShandruk <ulyana.shandruk@gmail.com>
2021-08-15 13:35:31 +0300

    ConsoleTestOOP

---
+++ 2021/ConsoleTestOOP/ConsoleTestOOP/ILogWriter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTestOOP
+{
+    public interface ILogWriter
+    {
+        void LogInfo(string message)
+        {
+
+        }
+        void LogWarning(string message)
+        {
+
+        }
+        void LogError(string message)
+        {
+
+        }
+    }
+}
---
+++ 2021/ConsoleTestOOP/ConsoleTestOOP/Model/Model.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTestOOP.Model
+{
+    public static class Model
+    {
+        public enum MessageTypes
+        {
+            Info,
+            Warning,
+            Error
+        }
+    }
+}
