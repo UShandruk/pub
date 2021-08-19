7ccd58b3672a41a5c00bb8b19ccbe3195a4ab63d
UShandruk <ulyana.shandruk@gmail.com>
2021-08-19 18:55:41 +0300

    02_MvcAndWebForms - первый коммит

---
+++ 2021/02_MvcAndWebForms/WebAppMVC/DAL/DAL.cs
@@ -0,0 +1,210 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAppMVC.Models;
+
+namespace WebAppMVC.DAL
+{
+    /// <summary>
+    /// Класс для работы с базой данных
+    /// </summary>
+    public static class DAL
+    {
+        //static string connectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=usersdb;Integrated Security=True";
+        static string connectionString = @"Data source = (LocalDB)\MSSQLLocalDB;Initial Catalog = DbHomeLibrary; Integrated Security = True";
+
+        /// <summary>
+        /// Получить список книг
+        /// </summary>
+        /// <returns></returns>
+        public static List<Book>GetBooks()
+        {
+            List<Book> listBook = new List<Book>();
+
+            // название хранимой процедуры
+            string sqlExpression = "sp_SelectAllBooks";
+            
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(sqlExpression, connection);
+                // указываем, что команда представляет хранимую процедуру
+                command.CommandType = System.Data.CommandType.StoredProcedure;
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {                        
+                        int id = reader.GetInt32(0);
+                        string author = reader.GetString(1);
+                        string name = reader.GetString(2);
+                        string content = reader.GetString(3);
+                        int year = reader.GetInt32(4);
+                        int numberOfVolumes = reader.GetInt32(5);
+
+                        Book book = new Book(id, author, name, content, year, numberOfVolumes);
+                        listBook.Add(book);
+                    }
+                }
+                reader.Close();
+            }
+
+            return listBook;
+        }
+
+        /// <summary>
+        /// Получить запись о книге
+        /// </summary>
+        public static Book LoadBook()
+        {
+            Book book = new Book(1,"","", "", 1,1);
+            return book;
+        }
+
+        /// <summary>
+        /// Добавить запись о книге
+        /// </summary>
+        //[HttpPost]
+        public static void AddBook(Book newBook)
+        {   
+            // название хранимой процедуры
+            string sqlExpression = "sp_InsertBook";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(sqlExpression, connection);
+                // указываем, что команда представляет хранимую процедуру
+                command.CommandType = System.Data.CommandType.StoredProcedure;
+
+                SqlParameter paramAuthor = new SqlParameter
+                {
+                    ParameterName = "@Author",
+                    Value = newBook.Author
+                };
+
+                SqlParameter paramName = new SqlParameter
+                {
+                    ParameterName = "@Name",
+                    Value = newBook.Name
+                };
+
+                SqlParameter paramContent = new SqlParameter
+                {
+                    ParameterName = "@Content",
+                    Value = newBook.Content
+                };
+
+                SqlParameter paramYear = new SqlParameter
+                {
+                    ParameterName = "@Year",
+                    Value = newBook.Year
+                };
+
+                SqlParameter paramNumberOfVolumes = new SqlParameter
+                {
+                    ParameterName = "@NumberOfVolumes",
+                    Value = newBook.NumberOfVolumes
+                };
+
+                command.Parameters.Add(paramAuthor);
+                command.Parameters.Add(paramName);
+                command.Parameters.Add(paramContent);
+                command.Parameters.Add(paramYear);
+                command.Parameters.Add(paramNumberOfVolumes);
+
+                var result = command.ExecuteNonQuery();
+            }
+        }
+
+        /// <summary>
+        /// Удалить запись о книге
+        /// </summary>
+        //[HttpPost]
+        public static void DeleteBook(int id)
+        {
+            // название хранимой процедуры
+            string sqlExpression = "sp_DeleteBook";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(sqlExpression, connection);
+                // указываем, что команда представляет хранимую процедуру
+                command.CommandType = System.Data.CommandType.StoredProcedure;
+
+                SqlParameter paramId = new SqlParameter
+                {
+                    ParameterName = "@Id",
+                    Value = id
+                };        
+                
+                command.Parameters.Add(paramId);
+
+                var result = command.ExecuteNonQuery();
+            }
+        }
+
+        /// <summary>
+        /// Обновить запись о книге
+        /// </summary>
+        //[HttpPost]
+        public static void UpdateBook(Book book)
+        {
+            // название хранимой процедуры
+            string sqlExpression = "sp_UpdateBook";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(sqlExpression, connection);
+                // указываем, что команда представляет хранимую процедуру
+                command.CommandType = System.Data.CommandType.StoredProcedure;
+
+                SqlParameter paramAuthor = new SqlParameter
+                {
+                    ParameterName = "@Author",
+                    Value = book.Author
+                };
+
+                SqlParameter paramName = new SqlParameter
+                {
+                    ParameterName = "@Name",
+                    Value = book.Name
+                };
+
+                SqlParameter paramContent = new SqlParameter
+                {
+                    ParameterName = "@Content",
+                    Value = book.Content
+                };
+
+                SqlParameter paramYear = new SqlParameter
+                {
+                    ParameterName = "@Year",
+                    Value = book.Year
+                };
+
+                SqlParameter paramNumberOfVolumes = new SqlParameter
+                {
+                    ParameterName = "@NumberOfVolumes",
+                    Value = book.NumberOfVolumes
+                };
+
+                command.Parameters.Add(paramAuthor);
+                command.Parameters.Add(paramName);
+                command.Parameters.Add(paramContent);
+                command.Parameters.Add(paramYear);
+                command.Parameters.Add(paramNumberOfVolumes);
+
+                var result = command.ExecuteNonQuery();
+            }
+            return;
+        }
+    }
+}
---
+++ 2021/02_MvcAndWebForms/WebAppMVC/Models/Book.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppMVC.Models
+{
+    /// <summary>
+    /// Книга
+    /// </summary>
+    [Table("books")]
+    public class Book
+    {
+        /// <summary>
+        /// Год издания
+        /// </summary>
+        [Key]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Aвтор
+        /// </summary>
+        public string Author { get; set; }
+
+        /// <summary>
+        /// Название
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Содержание (XML)
+        /// </summary>
+        public string Content { get; set; }
+
+        /// <summary>
+        /// Год издания
+        /// </summary>
+        public int Year { get; set; }
+
+        /// <summary>
+        /// Количество томов
+        /// </summary>
+        public int NumberOfVolumes { get; set; }
+
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        /// <param name="author">Aвтор</param>
+        /// <param name="name">Название</param>
+        /// <param name="content">Содержание (XML)</param>
+        /// <param name="year">Год издания</param>
+        public Book(int id, string author, string name, string content, int year, int numberOfVolumes)
+        {
+            Id = id;
+            Author = author;
+            Name = name;
+            Content = content;
+            Year = year;
+            NumberOfVolumes = numberOfVolumes;
+        }
+    }
+}
